30cf37176b9e04fd44b79b67e33df099f27ef664
Maks-Bax <rn4aey@yandex.ru>
2022-05-13 09:56:50 +0300

    ModCodJava

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 150;
-        int money = 500;
-        int bonus = money / 100;
-        if (money < 100) {
+        int balance = 100;
+        int money = 1100;
+        int bonus;
+        if (money < 1000) {
             bonus = 0;
         }
+        else bonus = money / 100;
         int sum = balance + money + bonus;
         System.out.println("Ваш бонус" + " " + bonus);
         System.out.println("Сумма на счету" + " " + sum);
